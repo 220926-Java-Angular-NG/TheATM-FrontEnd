db245750b219fd47aa587e915dcf76cbd5b68149
Justin Pan <105833041+jrprocks@users.noreply.github.com>
2022-11-04 16:18:31 -0400

    final changes

--- Revashare/src/app/components/register/register.component.ts
+++ Revashare/src/app/components/register/register.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
 
+export class RegisterComponent implements OnInit {
+  form: FormGroup;
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  registerForm = new FormGroup({
+    firstName: new FormBuilder(""),
+    lastName: new FormBuilder(""),
+    email: new FormBuilder(""),
+    password: new FormBuilder(""),
+    phoneNum: new FormBuilder(""),
+  });
+
+  registerSubmitted() {
+    console.log(this.registerForm);
   }
 
 }
