a51708f9986988b8df5780671a7c246fec045b8a
Justin Pan <105833041+jrprocks@users.noreply.github.com>
2022-11-07 03:49:47 -0500

    done with regsiter

--- Revashare/src/app/app-routing.module.ts
+++ Revashare/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
 
+
 const routes: Routes = [
   {path:"", redirectTo:'login', pathMatch:'full'},
   {path: 'register', component:RegisterComponent}
