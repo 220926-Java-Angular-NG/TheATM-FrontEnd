1da3f843869cc56441bf3e3a5db23e08defde691
unknown <vimiross00@gmail.com>
2022-11-10 20:01:34 -0700

    components routed to where they need to be

--- Revashare/src/app/app-routing.module.ts
+++ Revashare/src/app/app-routing.module.ts
@@ -4,14 +4,18 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
-
+import { ShoppingMallComponent } from './components/shopping-mall/shopping-mall.component';
+import { StatementsComponent } from './components/statements/statements.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
 
 const routes: Routes = [
   {path:"", redirectTo:'login', pathMatch:'full'},
   {path: 'register', component:RegisterComponent},
   {path: 'forgotpassword', component:ForgotPasswordComponent},
   {path: 'resetpassword', component:ResetPasswordComponent},
-  {path: 'dashboard', component:DashboardComponent}
+  {path: 'dashboard', component:DashboardComponent},
+  {path: 'shoppingMall', component:ShoppingMallComponent},
+  {path: 'userProfile', component:UserProfileComponent}
 ];
 
 @NgModule({
